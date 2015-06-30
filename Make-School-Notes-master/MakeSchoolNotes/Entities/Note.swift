6c90ed345296ba8db5861884f967d4db0edbc8a9
Arun Rawlani <arun.rawlani@mail.mcgill.ca>
2015-06-30 11:17:33 -0700

    Update Note.swift

--- Make-School-Notes-master/MakeSchoolNotes/Entities/Note.swift
+++ Make-School-Notes-master/MakeSchoolNotes/Entities/Note.swift
@@ -1,10 +1,4 @@
-//
-//  Note.swift
-//  MakeSchoolNotes
-//
-//  Created by Benjamin Encz on 2/13/15.
-//  Copyright (c) 2015 Make School. All rights reserved.
-//
+
 
 import Foundation
 import Realm

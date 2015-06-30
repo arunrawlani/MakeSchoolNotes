100d804aa432643da4e3129b2741f48cb35a06c4
Arun Rawlani <arun.rawlani@mail.mcgill.ca>
2015-06-30 11:16:17 -0700

    Update NoteDisplayViewController.swift

--- Make-School-Notes-master/MakeSchoolNotes/ViewControllers/NoteDisplayViewController.swift
+++ Make-School-Notes-master/MakeSchoolNotes/ViewControllers/NoteDisplayViewController.swift
@@ -1,10 +1,3 @@
-//
-//  NoteDisplayViewController.swift
-//  MakeSchoolNotes
-//
-//  Created by Benjamin Encz on 2/20/15.
-//  Copyright (c) 2015 Make School. All rights reserved.
-//
 
 import Foundation
 import UIKit

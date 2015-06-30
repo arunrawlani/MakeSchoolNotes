235e1b54925afc25d33ce7d4444a081dd325cf10
Arun Rawlani <arun.rawlani@mail.mcgill.ca>
2015-06-30 11:15:57 -0700

    Update NewNoteViewController.swift

--- Make-School-Notes-master/MakeSchoolNotes/ViewControllers/NewNoteViewController.swift
+++ Make-School-Notes-master/MakeSchoolNotes/ViewControllers/NewNoteViewController.swift
@@ -1,10 +1,3 @@
-//
-//  NewNoteViewController.swift
-//  MakeSchoolNotes
-//
-//  Created by Benjamin Encz on 2/13/15.
-//  Copyright (c) 2015 Make School. All rights reserved.
-//
 
 import UIKit
 import Realm
